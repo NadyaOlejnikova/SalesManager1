27e254963aec1cef5d993ae631a81218045edc80
nadyaOleynikova <kposhkanadya@yandex.ru>
2022-07-24 02:53:03 +0700

    averageWithoutMinAndMax

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,5 +1,6 @@
 public class SalesManager {
-    protected int[] sales = {100, 200, 300};
+
+    protected int[] sales = {15000, 500, 300, 100, 1000};
 
     public SalesManager() {
         this.sales = sales;
@@ -15,8 +16,32 @@ public class SalesManager {
         return max;
     }
 
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int average() {
+        int average = 0;
+        int count = 0;
+        int averageWithoutMinAndMax = 0;
+        for (int sale : sales) {
+            if (sale != min() && sale != max()) {
+                count++;
+                average += sale;
+                averageWithoutMinAndMax = average / count;
+            }
+        }
+        return averageWithoutMinAndMax;
+    }
 
     public String toString() {
-        return "Наиболее покумаемое количество товара  " + max();
+        return "Наиболее покумаемое количество товара  " + max() + "\n" + "Наимение покупаемое количество товара  " + min() + "\n"
+                + "Cреднее количество продаж среди всех продаж, за исключением самой максимальной и самой минимальной цифры " + average();
     }
 }
